f4b011425b78074148b939b0975ae8f0fa734aee
Richard Miller <richardmmill@gmail.com>
2021-03-29 22:13:22 -0400

    work on code-along 5.5

--- src/redux/hackerNewsReducer.js
+++ src/redux/hackerNewsReducer.js
@@ -1,32 +1,32 @@
 import axios from 'axios';
-import { response } from 'express';
+// import { response } from 'express';
 
 
 const initialState = {
     loading: false,
-    article: []
-};
+    articles: []
+}
 
 const REQUEST_ARTICLES = 'REQUEST_ARTICLES';
 
-function requestArticles() {
-    let articles = axios.get('/aip/hacker-news')
-        .then(response => response.data);
+export const requestArticles = () => {
+    let articles = axios.get('/api/hacker-news').then(res => res.data);
     return {
         type: REQUEST_ARTICLES,
         payload: articles
     }
 }
 
-export default function reducer(state = initialState, action) {
+export default function (state = initialState, action) {
     switch (action.type) {
         case REQUEST_ARTICLES + '_PENDING':
             return { ...state, loading: true };
         case REQUEST_ARTICLES + '_FULFILLED':
-            return { ...state, loading: false, articles: action.payload };
+            return { loading: false, articles: action.payload }
         case REQUEST_ARTICLES + '_REJECTED':
             return { ...state, loading: false }
-        default: return state;
+        default:
+            return state;
     }
-};
+}
 
